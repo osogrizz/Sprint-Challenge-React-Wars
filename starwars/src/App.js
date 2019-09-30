3e53364c273a1fb2337630194ee00a0e8884570f
osogrizz <lhtor@gmail.com>
2019-09-29 19:04:10 -0700

    Adds StarWars component. Fetches data for characters and displays one element for each character.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect  } from 'react';
 import './App.css';
+
+import axios from 'axios';
+import StarWars from './components/StarWars';
+import Page from './components/Page';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [character, setCharacter] = useState([]);
+  const [page, setPage] = useState('');
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    const fetchData = () => {
+      axios.get(`https://swapi.co/api/`)
+      .then(response => {
+        console.log(response.data.people);
+        axios.get(response.data.people)
+          .then(response => {
+            // console.log(response.data.results);
+            setCharacter(response.data.results);
+            console.log(response.data)
+
+            if (page === 'next') {
+              setCharacter(response.data.next);
+            }
+          })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+    fetchData();
+
+  },[page])
+  
+  
+
+
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <Page page={page}/>
+      <StarWars characters={character} />
     </div>
   );
 }
---
+++ starwars/src/components/Page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Page = (props) => {
+  console.log('page props', props);
+  return (
+    <div>
+      <button value="prev">prev</button>
+      <button value="next">next</button>
+    </div> 
+  )
+}
+
+export default Page;
