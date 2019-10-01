6ceb37f74aa384e10b87f3de89c29dbf70dd42b4
osogrizz <lhtor@gmail.com>
2019-09-30 19:06:16 -0700

    Adds pagination

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -3,13 +3,24 @@ import './App.css';
 
 import axios from 'axios';
 import StarWars from './components/StarWars';
-import Page from './components/Page';
+// import Page from './components/Page';
+
+import styled from 'styled-components';
+
+const Btns = styled.button`
+  width: 120px;
+  font-size: 1rem;
+  padding: 10px;
+  margin: 0 20px;
+`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   const [character, setCharacter] = useState([]);
-  const [page, setPage] = useState('');
+  const [next, setNext] = useState('');
+  const [prev, setPrev] = useState('');
+  const [page, setPage] = useState('page');
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -20,10 +31,11 @@ const App = () => {
         console.log(response.data.people);
         axios.get(response.data.people)
           .then(response => {
-            // console.log(response.data.results);
             setCharacter(response.data.results);
-            setPage(response.data)
-            console.log(response.data)
+            setNext(response.data.next);
+            setPrev(response.data.previous);
+            console.log('data', response.data)
+            
           })
       })
       .catch(err => {
@@ -34,13 +46,27 @@ const App = () => {
 
   },[])
 
-
+  useEffect(() => {
+    axios.get(`${page}`)
+      .then(response => {
+        setCharacter(response.data.results);
+        setPrev(response.data.previous)
+        setNext(response.data.next)
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [page])
+  
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      <Page page={page}/>
-      <StarWars characters={character} />
+      <div>
+        <Btns value="prev" onClick={ () => {setPage(prev)}} value={prev} >prev</Btns>
+        <Btns value="next" onClick={() => {setPage(next)}} value={next}>next</Btns>
+      </div>
+      <StarWars characters={character} page={page} />
     </div>
   );
 }
--- starwars/src/components/Page.js
+++ starwars/src/components/Page.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
 
 const Page = (props) => {
-  console.log('page props', props);
+  // console.log('page props', props.data.next);
+  const [page, setPage] = useState('');
+
+useEffect(() => {
+  axios.get(`${page}`)
+    .then( response => {
+      // console.log(response.data.results)
+    })
+    .catch( err => {
+      console.log(err);
+    })
+}, [page])
+
   return (
     <div>
-      <button value="prev" >prev</button>
-      <button value="next" >next</button>
+      <button value="prev" onClick={() => {setPage(props.data.previous)}} page={page}>prev</button>
+      <button value="next" onClick={() => {setPage(props.data.next)}}>next</button>
     </div> 
   )
 }
